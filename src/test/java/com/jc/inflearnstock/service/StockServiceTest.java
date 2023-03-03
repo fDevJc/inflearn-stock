68a21d98b678cfb723723f69aaeaa932a94e9be6
fDevJc <fdevjc@gmail.com>
2023-03-03 17:29:06 +0900

    동시성 문제

--- src/test/java/com/jc/inflearnstock/service/StockServiceTest.java
+++ src/test/java/com/jc/inflearnstock/service/StockServiceTest.java
@@ -1,6 +1,8 @@
 package com.jc.inflearnstock.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -31,14 +33,38 @@ class StockServiceTest {
 	void afterEach() {
 		stockRepository.deleteAll();
 	}
-	
+
 	@Test
 	void 재고감소() throws Exception {
-		stockService.decrease(savedStock.getId(),1L);
+		stockService.decrease(savedStock.getId(), 1L);
 
 		Stock ret = stockRepository.findById(savedStock.getId())
 			.orElseThrow();
 
 		Assertions.assertThat(ret.getQuantity()).isEqualTo(99L);
+	}
+
+	@Test
+	void 동시에_100개의_요청() throws Exception {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+
+		Stock stock = stockRepository.findById(savedStock.getId()).orElseThrow();
+
+		System.out.println("stock.getQuantity() = " + stock.getQuantity());
+		Assertions.assertThat(stock.getQuantity())
+			.isNotEqualTo(0L);
 	}
 }
