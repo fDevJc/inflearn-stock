3d98a8d45520c8720b5f3c68ce0683d4ed3ed0f4
fDevJc <fdevjc@gmail.com>
2023-03-03 16:53:53 +0900

    init commit

---
+++ src/main/java/com/jc/infleanstock/InfleanStockApplication.java
@@ -0,0 +1,13 @@
+package com.jc.infleanstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfleanStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfleanStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/infleanstock/InfleanStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.infleanstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
